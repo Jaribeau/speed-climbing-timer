7a2abd4ea921f53cda006a84127d2b5d7808779c
Jared Baribeau <jared.baribeau@gmail.com>
2019-08-03 14:20:09 +0700

    Initial setup of interrupts watching for switch presses

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # speed-climbing-timer
 Aruduino based self-timer rig for rock climbing, using foot sensors and a top-out sensor pad.
+
+
+### Interrupt info:
+http://gammon.com.au/interrupts
--- speed_climbing_timer/speed_climbing_timer.ino
+++ speed_climbing_timer/speed_climbing_timer.ino
@@ -1,33 +1,89 @@
+////////////////////////////////////////////////
+// Layout
+////////////////////////////////////////////////
+// Top: 
+//  12v, 5v, GND
+//  1x Laser
+//  1x light-sensor
+//  LEDs (TBD if digital IO or laser triggered)
+//  Large LCD?
+// Foot:
+//  1x Laser
+//  1x light-sensor
+//  1x LEDs
+
+////////////////////////////////////////////////
+// Modes
+////////////////////////////////////////////////
+// IDLE
+//  -> Show last score
+//  -> Press start
+
+// READY
+//  -> Timer set to zero
+//  -> Footswitch LED slow blinking (fading in/out?)
+//  -> "Step on PAD to start countdown"
+
+// COUNTDOWN
+//  -> Footswitch ON
+//  -> Double beep to signal countdown coming
+//  -> 5 second delay
+//  -> Beep beep BEEP
+//  -> If feet release: LED RED and BUZZ
+
+// CLIMBING
+//  -> Start timer (display on screen)
+//  -> Stop time when lazer hit (leave on screen)
+//  -> Back to IDLE
+
+
+
+/*
+TODO:
+- Plan out mode switching
+- Plan out LED control in the main loop
+ */
 
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
-#include <Mouse.h>
 
 
-//
 // Pin assignments
-//
-int LASER_PIN = 10;
-int LASER_SENSOR_PIN = 9;
-int POT_PIN = A0;
+int TOP_LASER_PIN = 10;
+int TOP_SENSOR_PIN = 9;
+int FOOT_LASER_PIN = 9;
+int FOOT_SENSOR_PIN = 9;
 
-// Variables
+//Constants
+const int IDLE      = 0;
+const int READY     = 1;
+const int COUNTDOWN = 2;
+const int CLIMBING  = 3;
+
+// State Variables
+int current_mode = 0;
 int counter = 0;
-int last_position = 0;
-bool already_incremented = false;
-
-// Mouse
-int prev_scroller_in = 0;
-long mouse_delay_timer = 0;
-long mouse_delay = 10;
+volatile bool footSwitchState = false;
+volatile bool topSwitchState = false;
 
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+
+
+void changeMode(int mode){
+
+}
+
+// Register new switch states
+void footSwitchPressedISR() { footSwitchState = true;   }
+void footSwitchReleasedISR(){ footSwitchState = false;  }
+void topSwitchPressedISR()  { topSwitchState = true;    }
+void topSwitchReleasedISR() { topSwitchState = false;   }
 
 
 void setup() {
   pinMode(LASER_SENSOR_PIN, INPUT);
   pinMode(LASER_PIN, OUTPUT);
-  pinMode(POT_PIN, INPUT);
 
   // Setup LCD
   lcd.init();
@@ -35,43 +91,26 @@ void setup() {
   lcd.setCursor(0,0);
   lcd.print("Hello!");
   lcd.setCursor(0,1);
-  lcd.print("Midi Pedal v0.1");
-
+  lcd.print("Climber Timer v0.1");
 
   //Serial setup
   Serial.begin(115200);
-  Serial.print("BtnAddr:");
+  Serial.print("Climber Timer v0.1");
 
-  // Start laser
-  digitalWrite(10, HIGH);
+  // Start lasers
+  digitalWrite(FOOT_LASER_PIN, HIGH);
+  digitalWrite(TOP_LASER_PIN, HIGH);
 
-  // Setup Mouse
-  Mouse.begin();
-  last_position = analogRead(POT_PIN);
+  // Setup input pins
+  pinMode(FOOT_SENSOR_PIN, INPUT_PULLUP);
+  pinMode(TOP_SENSOR_PIN, INPUT_PULLUP);
+
+  // Attach interrupt service routines (ISRs)
+  attachInterrupt(digitalPinToInterrupt(FOOT_SENSOR_PIN), footSwitchPressedISR, FALLING);
+  attachInterrupt(digitalPinToInterrupt(FOOT_SENSOR_PIN), footSwitchReleasedISR, RISING);
+  attachInterrupt(digitalPinToInterrupt(TOP_SENSOR_PIN), topSwitchPressedISR, FALLING);
+  attachInterrupt(digitalPinToInterrupt(TOP_SENSOR_PIN), topSwitchReleasedISR, RISING);
 }
 
 void loop() {  
-  if(digitalRead(LASER_SENSOR_PIN) && !already_incremented){
-    counter++;
-    already_incremented = true;
-  }
-  else if(!digitalRead(LASER_SENSOR_PIN) && already_incremented){
-    already_incremented = false; 
-    Serial.println("Mouse off!");
-  }
-
-////  Mouse.move(0,0,1);
-//  delay(100);
-//  Mouse.move(0,0, (char)(last_position - analogRead(POT_PIN)/8));
-//  last_position = analogRead(POT_PIN)/8;
-
-
-    ///////////////////
-  // MOUSE SCROLL WHEEL
-  ///////////////////
-  if(millis() - mouse_delay_timer > mouse_delay){
-    mouse_delay_timer = millis();
-    Mouse.move(0,0, (char)(prev_scroller_in - analogRead(POT_PIN)/8));
-    prev_scroller_in = analogRead(POT_PIN)/8;
-  }
 }
